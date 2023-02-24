7b7f9c5ff78c7c10380c03ab0dfdd804e3d7dbdc
georgi-rm <georgi.rm@gmail.com>
2023-02-24 23:56:13 +0200

    Problem 2 "Circumference And Surface Of Circle" from Lecture: Kotlin Introduction And Basic Syntax

---
+++ T01KotlinIntroductionAndBasicSyntax/P02CircumferenceAndSurfaceOfCircle/P02CircumferenceAndSurfaceOfCircle.kts
@@ -0,0 +1,15 @@
+import kotlin.math.PI
+
+/*
+* Task 2
+* Find circumference and surface of a circle
+* (r = 12.755)
+ */
+
+val radius: Double = 12.755
+
+val circumferenceOfCircle = 2 * radius * PI
+val surfaceOfCircle = PI * radius * radius
+
+circumferenceOfCircle
+surfaceOfCircle
